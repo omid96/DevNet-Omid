f40ab99c9e6405d51bb17f460553f566a4bc09f7
Omid Esmailbeig <omid.delta96@gmail.com>
2020-09-28 21:00:57 +0330

    Check if config is saved on Cisco

---
+++ configure_file_comparison.py
@@ -0,0 +1,69 @@
+import paramiko
+import telnetlib
+import time
+import re
+
+
+devices = [
+    { "HOST":"192.168.56.3", "PORT":5000 ,"hostname": "R1#"},
+    { "HOST":"192.168.56.3", "PORT":5001 ,"hostname": "R2#"}
+]
+
+
+# config_files = defaultdict()
+
+
+def read_terminal(telnet_connection, hostname):
+	time.sleep(5)
+	return telnet_connection.read_very_eager().decode('utf-8')
+
+
+def write_terminal(telnet_connection, string):
+	telnet_connection.write(bytes(string, 'utf-8'))
+
+
+def connect(host,port):
+	return telnetlib.Telnet(host,port)
+
+
+def terminal_prepare(telnet_connection):
+	write_terminal(telnet_connection,"\r\n")
+	write_terminal(telnet_connection,"terminal length 512 \r\n")
+	write_terminal(telnet_connection,"terminal width 512 \r\n")
+	write_terminal(telnet_connection,"\r\n")
+
+
+if __name__ == "__main__":
+
+    for device in devices:
+
+        print("-" * 20, device['hostname'].split('#')[0], "-" * 20)
+        telnet_connection = connect(device["HOST"], device["PORT"])
+        terminal_prepare(telnet_connection)
+
+        write_terminal(telnet_connection, "show archive config difference \r\n")
+        output = read_terminal(telnet_connection, device["hostname"])
+
+        match_object = re.search(r'No changes were found', output, re.DOTALL)
+        if not match_object:
+            prompt = input("Config need to be saved. Do you want to save it? (y,n): ")
+            if prompt.lower() in ['y', 'yes']:
+                write_terminal(telnet_connection, "wr \r\n")
+                print("Config saved!!!")
+            else:
+                print("Skip saving config")
+        else:
+            print("There are no new changes in config")
+
+        # write_terminal(telnet_connection, "show startup-config \r\n")
+        # startup_config = read_terminal(telnet_connection, device["hostname"])
+        # startup_config_hash = hashlib.md5(bytes(startup_config, encoding='utf-8'))
+        # print(startup_config_hash.digest())
+
+        # print(lines)
+        # for line in lines:
+        #     print ( line )
+
+        telnet_connection.close()
+
+    exit()
